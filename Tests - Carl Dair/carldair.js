0bcfe21ff8186699df8e64669995a7f2f2c61189
vilmorin <42790639+vilmorin@users.noreply.github.com>
2018-12-04 18:55:05 -0500

    Bike Journey

---
+++ Tests - Carl Dair/carldair.js
@@ -0,0 +1,7 @@
+var question = (prompt("Why aren't koalas actual bears"));
+
+if (question == "they dont meet the koalafications") {
+	alert("Hey, how did you know?");
+} else {
+	alert("Because they don't meet the koalafications");
+}
